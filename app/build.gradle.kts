cdc4b1ea7ab970b82601ab65e02e2d7a7ffd8e0f
Marcel Reiter <m.reiter@anfe.ma>
2023-08-03 16:31:45 +0200

    Enhancement: add kotlinx-datetime to depndencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ android {
 }
 
 dependencies {
-
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
